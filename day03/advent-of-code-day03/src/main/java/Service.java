b0e76e419b3c1ad7e6269c7f6c6195db7360a907
lkeleti <keleti.laszlo@vipmail.hu>
2025-12-03 11:59:37 +0100

    Day03 Part 1 completed

--- day03/advent-of-code-day03/src/main/java/Service.java
+++ day03/advent-of-code-day03/src/main/java/Service.java
@@ -2,19 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Service {
+    private final List<String> numbers = new ArrayList<>();
+
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             while ((line = br.readLine()) != null) {
+                numbers.add(line);
             }
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
         }
     }
+    public NumPos findLargestNumber(int start, int end, String number) {
+        int maxNumber = -1;
+        int maxPosition = -1;
+        for (int i= start; i < end; i++) {
+            int defDigit = Integer.parseInt(number.substring(i,i+1));
+            if (defDigit > maxNumber ) {
+                maxNumber = defDigit;
+                maxPosition = i;
+            }
+        }
+        return new NumPos(maxNumber,maxPosition);
+    }
     public long partOne() {
-        return 0;
+        Long sumOfNumbers = 0L;
+        for (String number: numbers) {
+            NumPos first = findLargestNumber(0, number.length() - 1, number);
+            NumPos secund = findLargestNumber(first.getPosition() + 1, number.length(), number);
+            sumOfNumbers += (first.getNumber() * 10L) + secund.getNumber();
+        }
+        return sumOfNumbers;
     }
     public int partTwo() {
         return 0;
